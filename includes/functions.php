678ae779064f0ca311163817b098b22bbc0fd4f5
Alex Crocker <croc122@gmail.com>
2016-03-21 18:16:30 -0400

    did stuff

--- includes/functions.php
+++ includes/functions.php
@@ -3,15 +3,20 @@
 $orlandofeed = file_get_contents("http://api.wunderground.com/api/67841e7ca89044b1/conditions/q/FL/Orlando.xml");
 $tampafeed = file_get_contents("http://api.wunderground.com/api/67841e7ca89044b1/conditions/q/FL/Tampa.xml");
 $miamifeed = file_get_contents("http://api.wunderground.com/api/67841e7ca89044b1/conditions/q/FL/Miami.xml");
+$pensacolafeed = file_get_contents("http://api.wunderground.com/api/67841e7ca89044b1/conditions/q/FL/Pensacola.xml");
+$destinfeed = file_get_contents("http://api.wunderground.com/api/67841e7ca89044b1/conditions/q/FL/Destin.xml");
 
 $orlandoxml = simplexml_load_string($orlandofeed);
 $tampaxml = simplexml_load_string($tampafeed);
 $miamixml = simplexml_load_string($miamifeed);
+$pensacolaxml = simplexml_load_string($pensacolafeed);
+$destinxml = simplexml_load_string($destinfeed);
 
 $orlandoxml->asXML('data/Orlando.xml');
 $tampaxml->asXML('data/Tampa.xml');
 $miamixml->asXML('data/Miami.xml');
-
+$pensacolaxml->asXML('data/Pensacola.xml');
+$destinxml->asXML('data/Destin.xml');
 
 function fetch($x) {
 	$temp = simplexml_load_file('data/' . $x . '.xml')->current_observation->temp_f . "&deg;F";
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-$(document).ready({
-   // nothing
-});
+
 
 $("#header").click(function() {
     $("#nav").slideToggle("slow", function() {
