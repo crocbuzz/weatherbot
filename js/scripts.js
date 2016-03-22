bfde4adf09d3fbdd8c9e6f56a258cbf07391d90e
Alex Crocker <croc122@gmail.com>
2016-03-21 20:03:55 -0400

    did even more stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+/*
+JQUERY4U = {
+	multiply: function(x,y) {
+		return (x * y);
+	}
+}
+//function call
+console.log(JQUERY4U.multiply(2,2));
+*/
+
+$("select").change(function() {
+	$.ajax({
+		url: "data/Orlando.xml", 
+		success: function(xml) {
+			var weather = $(xml).find('weather').text();
+			var temperature_string = $(xml).find('temperature_string').text();
+			var relative_humidity = $(xml).find('relative_humidity').text();
+			var wind_string = $(xml).find('wind_string').text();
+			alert(data);
+		},
+		error: function() {
+			alert("it didn't work");
+		}
+	});
+});
 
 
 $("#header").click(function() {
