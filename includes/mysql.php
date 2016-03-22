099b5ce2e0c88e093635997a7eeb3ca2bb379787
Alex Crocker <croc122@gmail.com>
2016-03-22 15:16:39 -0400

    mysql stuff

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,17 +1,28 @@
 <?php
 
-	//Checking to see if data exists. If it doesn't, it creates it.
+
 		$ip = $_SERVER['REMOTE_ADDR'];
 
 		$sql = "SELECT * FROM weatherbot WHERE ipaddress='" . $ip . "'";
 		$result = mysqli_query($connection, $sql);
-        $users = mysqli_fetch_all($result);
-		if (in_array($ip, $users)){
-			$xyz =  "Welcome back!";
-		} else {
-			$sql = "INSERT INTO weatherbot (ipaddress) VALUES(" . $ip . ")";
-			mysqli_query($connection, $sql);
-			$xyz = "Your IP address has been logged.";
-		}
+				
+			if ($result != NULL) {
+				$users = mysqli_fetch_assoc($result);
+				if (is_array($users)) {
+			        if (!in_array($ip, $users, true)) {
+			            $users->users[] = $users;
+			            $sql = "INSERT INTO weatherbot (ipaddress) VALUES(" . $ip . ")";
+						mysqli_query($connection, $sql);
+						$xyz = "Your IP address has been logged.";
+			        } else {
+				        $xyz =  "Welcome back!";
+			        }
+			    }
+			} else {
+				$sql = "INSERT INTO weatherbot (ipaddress) VALUES(" . $ip . ")";
+				mysqli_query($connection, $sql);
+				$xyz = "Your IP address has been logged.";
+			}
+				
 
 ?>
