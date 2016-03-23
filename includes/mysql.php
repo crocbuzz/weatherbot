aff08848a0cf213c74625b9ede273b920fe4e512
Alex Crocker <croc122@gmail.com>
2016-03-23 10:56:04 -0400

    re-worked mysql.php

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,28 +1,29 @@
 <?php
 
 
-		$ip = $_SERVER['REMOTE_ADDR'];
+	$ip = $_SERVER['REMOTE_ADDR'];
 
-		$sql = "SELECT * FROM weatherbot WHERE ipaddress='" . $ip . "'";
-		$result = mysqli_query($connection, $sql);
-				
-			if ($result != NULL) {
-				$users = mysqli_fetch_assoc($result);
-				if (is_array($users)) {
-			        if (!in_array($ip, $users, true)) {
-			            $users->users[] = $users;
-			            $sql = "INSERT INTO weatherbot (ipaddress) VALUES(" . $ip . ")";
-						mysqli_query($connection, $sql);
-						$xyz = "Your IP address has been logged.";
-			        } else {
-				        $xyz =  "Welcome back!";
-			        }
-			    }
-			} else {
-				$sql = "INSERT INTO weatherbot (ipaddress) VALUES(" . $ip . ")";
+	$sql = "SELECT * FROM weatherbot WHERE ipaddress='" . $ip . "'";
+	$result = mysqli_query($connection, $sql);
+			
+		$users = mysqli_fetch_assoc($result);
+		if (is_array($users)) {
+	        if (!in_array($ip, $users, true)) {
+		        print_r($users);
+	            $users->users[] = $users;
+	            $sql = "INSERT INTO weatherbot (ipaddress) VALUES(" . $ip . ")";
 				mysqli_query($connection, $sql);
 				$xyz = "Your IP address has been logged.";
-			}
+	        } else {
+		        $xyz =  "Welcome back!";
+	        }
+	    } else {
+		
+			$sql = "INSERT INTO weatherbot (ipaddress) VALUES(" . $ip . ")";
+			mysqli_query($connection, $sql);
+			$xyz = "Your IP address has been logged.";
+		
+		}
 				
 
 ?>
